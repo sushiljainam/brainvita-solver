9357f23772715bc30cd18ad71ff7c49e97ff28fb
Sushil Jain <13596814+sushiljainam@users.noreply.github.com>
2022-08-19 12:55:49 +0530

    draft - print matrix

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+const boardSize = 7;
+const CENTER = { x: 3, y: 3 };
+const EMPTYS = [{ x: 3, y: 3 }];
+const VOID_BEYOND_N_OF_CENTER = 2; // to form plus shape of width 3
+const EMPTY_NODE = 'empty';
+const VOID_NODE = 'void';
+const FILLED_NODE = 'filled';
+
+function attemptAllSolutions() {
+    let discardedCount = 0;
+    let gameMatrix = [];
+    for (let i = 0; i < boardSize; i++) {
+        gameMatrix.push([]);
+        for (let j = 0; j < boardSize; j++) {
+            gameMatrix[i].push(fillNode(i, j));
+        }
+    }
+    printBoard(gameMatrix);
+}
+
+function fillNode(i, j) {
+    // FILL ALL EMPTY
+    for (let k = 0; k < EMPTYS.length; k++) {
+        if (EMPTYS[k].x === i && EMPTYS[k].y === j) {
+            return EMPTY_NODE;
+        }
+    }
+    // FILL ALL VOIDS
+    if (i <= Math.abs(CENTER.x - VOID_BEYOND_N_OF_CENTER) && j <= Math.abs(CENTER.y - VOID_BEYOND_N_OF_CENTER)) {
+        return VOID_NODE;
+    }
+    return FILLED_NODE;
+}
+
+function printBoard(mat) {
+    for (let i = 0; i < mat.length; i++) {
+        const row = mat[i];
+        let str = ''
+        for (let j = 0; j < row.length; j++) {
+            const node = row[j];
+            if (node === VOID_NODE) {
+                str += '  '
+            } else if (node === EMPTY_NODE) {
+                str += '0 '
+            } else if (node === FILLED_NODE) {
+                str += '1 '
+            }
+        }
+        console.log(str);
+    }
+}
+
+attemptAllSolutions();
+
