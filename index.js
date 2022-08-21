ee5d55df7ad7a7e8a1d966ff74611d6c19aa57af
Sushil Jain <13596814+sushiljainam@users.noreply.github.com>
2022-08-21 23:09:50 +0530

    NEXT PLAN v2

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const boardSize = 7;
 
 attemptAllSolutions(boardSize);
 runSavedSolution(boardSize, { remaining: 2 });
+// pass saver fn to attemptAllSolutions
+// build or import matrix symmetry checks
+// save all matrix as one-string in DB,
+// label them for symmetry and run ALL solutions (2^30 around)
+// via dynamic optimizations and asynchronous/ discontinued execution approach
+// due to solutions are saved in DB
