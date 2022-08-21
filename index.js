229ebb5240d23b8cd8cdf1116c7ed287e03037ca
Sushil Jain <13596814+sushiljainam@users.noreply.github.com>
2022-08-21 12:16:37 +0530

    remove unused imports

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const { clone, concat, includes } = require("ramda");
-const { refillMatrix, runSpecificPath, printBoard, getAllPaths } = require("./lib/common");
+
 const { runSavedSolution } = require("./lib/printSavedSolution");
 const { attemptAllSolutions } = require("./lib/runAllSolutions");
-const { saveToDb, closeDb, getSaved } = require("./lib/saveToDb");
-
 
 const boardSize = 7;
 
 attemptAllSolutions();
-// runSavedSolution(boardSize, { remaining: 2 });
+runSavedSolution(boardSize, { remaining: 2 });
