30f1e215bfd6b3aaec7df52d69ddd600c23e1779
Sushil Jain <13596814+sushiljainam@users.noreply.github.com>
2022-08-27 13:59:11 +0530

    adding more test cases

--- test/testSymmetry.js
+++ test/testSymmetry.js
@@ -1,7 +1,7 @@
 const { deepEqual } = require('assert');
 const { findAllSymmetricMetrices, findUniqueMatrices } = require("../lib/findSymmetry");
 
-const output = findAllSymmetricMetrices([[1, 2], [3, 4]])
+const output = findAllSymmetricMetrices([[1, 2], [3, 4]]);
 deepEqual(output, [
     [[1, 2], [3, 4]],
     [[3, 1], [4, 2]],
@@ -10,7 +10,7 @@ deepEqual(output, [
     [[1, 3], [2, 4]],
     [[2, 1], [4, 3]],
     [[4, 2], [3, 1]],
-    [[3, 4], [1, 2]]
+    [[3, 4], [1, 2]],
 ], 'findAllSymmetricMetrices not working fine');
 
 const output2 = findUniqueMatrices([
@@ -19,6 +19,16 @@ const output2 = findUniqueMatrices([
 ]);
 deepEqual(output2, [
     [[1, 2], [3, 4]],
+]);
+
+deepEqual(findUniqueMatrices([
+    [[4, 3], [2, 1]],
+    [[2, 4], [1, 3]],
+    [[1, 3], [2, 4]],
+    [[2, 1], [4, 3]],
+    [[4, 2], [3, 1]],
+]), [
+    [[4, 3], [2, 1]],
 ]);
 
 console.log('All good!');
