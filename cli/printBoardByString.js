b37bab5f0453c012558e3160e6e6e60c49636ad7
Sushil Jain <13596814+sushiljainam@users.noreply.github.com>
2022-09-18 20:48:53 +0530

    do same thing in loop

--- cli/printBoardByString.js
+++ cli/printBoardByString.js
@@ -1,8 +1,15 @@
 const prompt = require("prompt-sync")({ sigint: true });
 const { printBoard, boardStringToBoard } = require("../lib/common");
 
-const st = prompt("Paste the boardString ..\n");
-
+function stringToContinue() {
+    const st = prompt("Paste the boardString :- ");
+    if (st.length === 49) {
+        printBoard(boardStringToBoard(st));
+        stringToContinue();
+    } else {
+        console.log('incorrect string input');
+    }
+}
+stringToContinue();
 // const st = await prompt('type board string?')
-printBoard(boardStringToBoard(st));
 // printBoard(boardStringToBoard(process.argv[2]));
